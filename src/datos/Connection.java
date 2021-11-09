a0da89fea4ea44a939abec45caf08d4f2a6d628d
alexa <leyva.garcia.diego.alejandro@gmail.com>
2021-11-09 13:51:16 -0600

    Clase de conexión a la base de datos

---
+++ src/datos/Connection.java
@@ -0,0 +1,51 @@
+package datos;
+
+import java.sql.*;
+
+public class Connection {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/mirantibus_db?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "admin";
+
+    public static java.sql.Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void close(ResultSet resultSet) {
+        try {
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(CallableStatement callableStatement) {
+        try {
+            callableStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(PreparedStatement preparedStatement) {
+        try {
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(java.sql.Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
