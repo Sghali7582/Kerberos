263db7f7b96ee706767d90b64fd498b3d5e9bccf
Silver-VS <vacevess04@gmail.com>
2021-11-20 23:11:27 -0600

    Create new class for Tickets.java.

---
+++ src/dominio/tickets/Ticket.java
@@ -0,0 +1,68 @@
+package dominio.tickets;
+
+public class Ticket {
+
+    private String idTicket;
+    private String firstId;
+    private String secondId;
+    private String addressIP;
+    private String lifetime;
+    private String timeStamp;
+    private String key;
+
+    public String getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(String idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public String getFirstId() {
+        return firstId;
+    }
+
+    public void setFirstId(String firstId) {
+        this.firstId = firstId;
+    }
+
+    public String getSecondId() {
+        return secondId;
+    }
+
+    public void setSecondId(String secondId) {
+        this.secondId = secondId;
+    }
+
+    public String getAddressIP() {
+        return addressIP;
+    }
+
+    public void setAddressIP(String addressIP) {
+        this.addressIP = addressIP;
+    }
+
+    public String getLifetime() {
+        return lifetime;
+    }
+
+    public void setLifetime(String lifetime) {
+        this.lifetime = lifetime;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
