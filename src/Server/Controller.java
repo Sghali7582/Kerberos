e719a49dccd63c3f0e4837c588ba6808f97d43a5
Silver-VS <vacevess04@gmail.com>
2021-11-23 18:54:10 -0600

    Start actions and responses in the Server

---
+++ src/Server/Controller.java
@@ -0,0 +1,19 @@
+package Server;
+
+import Comms.Messenger;
+
+import java.net.ServerSocket;
+
+public class Controller {
+    public static void main(String[] args) {
+        Messenger messenger = new Messenger();
+        ServerSocket serverSocket = messenger.initServerSocket(5502);
+        System.out.println("Servidor iniciado y esperando peticiones...");
+
+
+        do {
+            new ProcessRequest().processUserRequest(messenger.acceptRequest(serverSocket));
+            System.out.println("Respuesta enviada al cliente.");
+        } while (!serverSocket.isClosed());
+    }
+}
