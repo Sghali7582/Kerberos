4331ba0a2cd80e6ef9748ed6fe2e93461185c84f
Silver-VS <vacevess04@gmail.com>
2021-11-23 19:04:59 -0600

    Make database connection simpler to understand.

--- src/datos/Connection.java
+++ src/datos/Connection.java
@@ -1,19 +1,52 @@
 package datos;
 
-import java.sql.*;
+import java.sql.DriverManager;
+
+/**
+ * @author Silver-VS
+ */
 
 public class Connection {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/mirantibus_db?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "admin";
 
-    public static java.sql.Connection getConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+    private java.sql.Connection con;
+
+    public java.sql.Connection getCon() {
+        return con;
+    }
+
+    public void setCon(java.sql.Connection con) {
+        this.con = con;
+    }
+
+    public void connect(String databaseName) throws Exception {
+        //We look for the database driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //We establish the credentials to access the database
+        String databasePassword = "";
+        String databaseUser = "root";
+
+        //We use the database URL with its configurations.
+        String databaseURL =
+                "jdbc:mysql://" + //MySQL database.
+                        "localhost:3306/" + //Location of database (host:port).
+                        databaseName +  //Database name
+                        "?useSSL=false" + //Secure Socket Layout, we need a certificate in order to use this.
+                        "&useUnicode=true" + //Encoding for the database.
+                        "&useJDBCCompliantTimezoneShift=true" + //Avoid problems with time zones.
+                        "&useLegacyDatetimeCode=false" + //No legacy required.
+                        "&serverTimezone=UTC" + //Self-explanatory.
+                        "&allowPublicKeyRetrieval=true"; //Self-explanatory.
+
+        //We create the connection with the URL and the credentials to access.
+        con = DriverManager.getConnection(databaseURL, databaseUser, databasePassword);
+    }
+
+    public void disconnect() throws Exception {
+        if (con != null) {
+            if (!con.isClosed()) {
+                con.isClosed();
+            }
         }
-        return null;
     }
 }
